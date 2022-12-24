b6e8d84411bf438afc2f3b09638d306220ca34aa
Arce <77249136+arcesoftware@users.noreply.github.com>
2022-12-23 23:14:40 -0600

    Alura Challenge1

---
+++ js/funcionalidad.js
@@ -0,0 +1,96 @@
+var msj = document.querySelector("#textarea"); /*agarra el texto del textarea y lo envia a la funcion encriptar*/
+var btnEncriptar = document.querySelector("#encriptar"); /*btn encriptar*/
+var btnDesencriptar = document.querySelector("#desencriptar"); /*btn desencriptar*/
+var btnCopiar = document.querySelector("#copiar"); /*btn encriptar*/
+var msjFinal = document.querySelector("#resultado"); /*text area de resultado*/
+
+
+/*Funcion encriptar*/
+function msjEncriptar(txt) {
+    if (txt == "a") {
+        txt = "ia";
+        return txt;
+    } else if (txt == "e") {
+        txt = "enter";
+        return txt;
+    } else if (txt == "i") {
+        txt = "imes";
+        return txt;
+    } else if (txt == "o") {
+        txt = "ober";
+        return txt;
+    } else if (txt == "u") {
+        txt = "ufat";
+        return txt;
+    }
+}
+
+function encriptar(txt) {
+    var msjEncriptado = txt.replace(/a|e|i|o|u/g, msjEncriptar); /*reemplaza las letras introducidas por la funcion msjEncriptar*/
+    let noEncontrado = document.getElementById('ocultar');
+    let mensajeFinal = document.getElementById('resultado');
+    let copiar = document.getElementById('copiar');
+    let enter = document.getElementById('encriptar');
+    noEncontrado.style.display = "none";
+    mensajeFinal.style.display = 'flex';
+    copiar.style.display = 'block';
+    return msjEncriptado;
+}
+
+/*funcion desencriptar*/
+
+function msjDesencriptar(txt) {
+    if (txt == "ia") {
+        txt = "a";
+        return txt;
+    } else if (txt == "enter") {
+        txt = "e";
+        return txt;
+    } else if (txt == "imes") {
+        txt = "i";
+        return txt;
+    } else if (txt == "ober") {
+        txt = "o";
+        return txt;
+    } else if (txt == "ufat") {
+        txt = "u";
+        return txt;
+    }
+    var encrypted = CryptoJS.RC4Drop.encrypt("textarea", "Secret Passphrase");
+
+    var encrypted = CryptoJS.RC4Drop.encrypt("Mestextareasage", "Secret Passphrase", {
+        drop: 3072 / 4
+    });
+    var decrypted = CryptoJS.RC4Drop.decrypt(encrypted, "Secret Passphrase", {
+        drop: 3072 / 4
+    });
+}
+
+function desencriptar(txt) {
+    var msjDesencriptado = txt.replace(/ia|enter|imes|ober|ufat/g, msjDesencriptar);
+    if (txt === msjEncriptar) return msjFinal
+    else return msjDesencriptado;
+}
+
+
+btnEncriptar.addEventListener("click", function (event) {
+    event.preventDefault();
+    var txt = msj.value;
+    var msjSeguro = encriptar(txt);
+    msjFinal.value = msjSeguro;
+    console.log(msjSeguro);
+});
+
+btnDesencriptar.addEventListener("click", function (event) {
+    event.preventDefault();
+    var txt = msj.value;
+    var msjSeguro = desencriptar(txt);
+    msjFinal.value = msjSeguro;
+    console.log(msjSeguro);
+});
+
+btnCopiar.addEventListener("click", function (event) {
+    event.preventDefault();
+    msjFinal.select();
+    navigator.clipboard.writeText(msjFinal.value);
+});
